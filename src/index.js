8a67cd02e8e4257d19c7dd5294b47faf7bed7d94
Ashe Urban <ashe@goldentongue.com>
2022-07-02 15:30:50 -0700

    ADD html to display gif.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-// $(document).ready(function () {
-//   $('selector').submit(function (event)) {
-//     event.preventDefault(); //if it's a form
-//     // UI code here
-//   }
-// });
+$(document).ready(function() {
+  $('#stringQuery').click(function() {
+    const search = $('#q').val();
+    $('#q').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=25&offset=0&rating=g&lang=en`;
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        showGif(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function showGif(response) {
+      $('.showGif').html(`<img class="m-3 rounded mx-auto my-auto d-block" src="${response.data[0].images.original.url}" />`);
+    }
+
+  });
+});
