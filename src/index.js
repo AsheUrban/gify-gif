ac7b9a4ef66a0bc72a8427a8e457516b2195cc55
Ashe Urban <ashe@goldentongue.com>
2022-07-02 13:15:30 -0700

    SET UP initial project environment.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function () {
-  $('selector').submit(function (event)) {
-    event.preventDefault(); //if it's a form
-    // UI code here
-  }
-});
+// $(document).ready(function () {
+//   $('selector').submit(function (event)) {
+//     event.preventDefault(); //if it's a form
+//     // UI code here
+//   }
+// });
